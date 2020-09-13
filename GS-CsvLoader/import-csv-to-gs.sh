e3a662cde49a8ca8104f55ce80a39751a8cf680d
aharonmoll <aharon@gigaspaces.com>
2020-09-13 12:42:54 +0300

    add import csv to gs script

--- GS-CsvLoader/import-csv-to-gs.sh
+++ GS-CsvLoader/import-csv-to-gs.sh
@@ -4,7 +4,7 @@ export GS_LOOKUP_LOCATORS="localhost"
 export GS_SPACE_NAME="demo"
 export CSV_FILE="src/main/resources/ibm.us.csv"
 export CSV_POJO="com.gs.csm.data.MyNewPojo"
-export NUM_OF_ITERATIONS=1;
+export NUM_OF_ITERATIONS=5;
 
 ./build.sh
 mvn exec:java  -Dexec.mainClass=com.gs.csm.LoadCSVData -Dexec.classpathScope=compile -Dexec.args=$GS_SPACE_NAME
