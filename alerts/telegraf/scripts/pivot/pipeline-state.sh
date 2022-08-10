c439408f58bafdb477edc1823f19528f14859e6d
Tapan89-ux <76691807+Tapan89-ux@users.noreply.github.com>
2022-08-10 18:23:52 +0530

    Update pipeline-state.sh
    updated missing part

--- alerts/telegraf/scripts/pivot/pipeline-state.sh
+++ alerts/telegraf/scripts/pivot/pipeline-state.sh
@@ -2,7 +2,11 @@
 # /usr/local/bin/pipeline-state.sh
 
 # Query DB2 pipeline reference table for last update time, time should be UTC, convert it to POSIX nanosecond EPOCH time.
-echo "pipelineState,pipeline=db2 last=$(date '+%s')000000000"
+O=$(/usr/local/bin/test.sh 2>/dev/null)
+[ $? -ne 0 ] && O='2000-01-01 00:00:00'
+D=$(echo "$O" | grep -v logger)
+
+echo "pipelineState,pipeline=db2 last=$(date -d "$D" '+%s')000000000"
 
 # Query MSSQL pipeline reference table for last update time, time should be UTC, convert it to POSIX EPOCH time.
 echo "pipelineState,pipeline=mssql last=$(date '+%s')000000000"
