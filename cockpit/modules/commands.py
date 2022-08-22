e8a507e5a6d8ef03d8f5fdd941de5a815cee39c9
segalalon <segalalon@gmail.com>
2022-08-22 16:09:35 +0300

    initial commit: sample for modules

---
+++ cockpit/modules/commands.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# *-* coding: utf-8 *-*
+
+def list_tasks():
+    print("listing tasks ...")
+    input("press ENTER to continue")
+
+def show_task():
+    list_tasks()
+    print("showing selected task ...")
+    input("press ENTER to continue")
