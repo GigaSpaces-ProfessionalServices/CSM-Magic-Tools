73738b5c4a88cb79306a7285eac59ed87fc73a10
segalalon <segalalon@gmail.com>
2022-09-01 15:22:05 +0300

    added records count

--- cockpit/scripts/db_info.py
+++ cockpit/scripts/db_info.py
@@ -4,6 +4,13 @@
 import os
 import yaml
 import sqlite3
+from signal import SIGINT, signal
+
+
+
+def handler(signal_recieved, frame):
+    print('\n\nOperation aborted by user!')
+    exit(0)
 
 
 def create_connection(db_file):
@@ -26,21 +33,33 @@ def list_db_tables(conn):
     c.execute("SELECT name FROM sqlite_master WHERE type='table';")
     tables = c.fetchall()
     if len(tables) > 0:
-        print("cockpit database tables: ")
+        print("\n[Cockpit database tables]")
         for table_name in tables:
-            print(f" - {table_name[0]}")
+            c.execute(f"SELECT count(*) FROM {table_name[0]};")
+            num_records = f"{len(c.fetchall())} record(s)"
+            print(f"   {table_name[0]:<6} : {num_records:<10}")
         c.close()
     conn.close()
 
+
+# main #
 config_yaml = f"{os.path.dirname(os.path.abspath(__file__))}/../config/config.yaml"
+
+# catch user CTRL+C key press
+signal(SIGINT, handler)
 
 # load yaml
 with open(config_yaml, 'r') as o:
     data = yaml.safe_load(o)
 
-db_home = data['params']['cockpit']['db_home']
-db_file = f"{db_home}/cockpit.db"
+cockpit_db_home = data['params']['cockpit']['db_home']
+cockpit_db_name = data['params']['cockpit']['db_name']
+cockpit_db = f"{cockpit_db_home}/{cockpit_db_name}"
 
-print(f"Cockpit DB location: {db_file}")
-list_db_tables(create_connection(db_file))
-input("\nPress ENTER to continue")
+print(f"[Cockpit DB location]\n   {cockpit_db}\n")
+conn = create_connection(cockpit_db)
+if conn is not None:
+    list_db_tables(conn)
+else:
+    print("ERROR: unable to establish database connection.")
+input("\nPress ENTER to return to main menu")
