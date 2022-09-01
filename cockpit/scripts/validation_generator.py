6cac11b2972bd91c3ef20dd513d75397892a4241
segalalon <segalalon@gmail.com>
2022-09-01 16:24:02 +0300

    chanes to get_selection function and db connection

--- cockpit/scripts/validation_generator.py
+++ cockpit/scripts/validation_generator.py
@@ -11,6 +11,7 @@ import datetime
 from signal import SIGINT, signal
 import subprocess
 
+
 def handler(signal_recieved, frame):
     print('\n\nOperation aborted by user!')
     exit(0)
@@ -28,48 +29,32 @@ def get_object_types():
 
 
 def validate_input(items_dict):
-        from colorama import Fore
-        choice = input("\nEnter your choice: ")
-        while True:
-            if choice == '99':
-                return -1
-            if len(items_dict) > 1:
-                if choice == str(len(items_dict) + 1): # if 'ALL' is selected
-                    return "ALL"
-            if not choice.isdigit() or int(choice) not in items_dict.keys():
-                choice = input(f"{Fore.RED}ERROR: Input must be a menu index!{Fore.RESET}\nEnter you choice: ")
-            else:
-                return int(choice)
+    from colorama import Fore
+    choice = input("\nEnter your choice: ")
+    while True:
+        if choice == '99':
+            return -1
+        if len(items_dict) > 1:
+            if choice == str(len(items_dict) + 1): # if 'ALL' is selected
+                return "ALL"
+        if not choice.isdigit() or int(choice) not in items_dict.keys():
+            choice = input(f"{Fore.RED}ERROR: Input must be a menu index!{Fore.RESET}\nEnter you choice: ")
+        else:
+            return int(choice)
 
 
-def get_selected_type(types):
+def get_selection(the_dict, description):
     # print menu
-    q = "Which type would you like to validate?"
+    q = f"Which {description} would you like to validate?"
     print(q + "\n" + '=' * len(q))
-    for k, v in types.items():
+    for k, v in the_dict.items():
         index = f"[{k}]"
         print(f'{index:<4} - {v[0]:<24}')
-    if len(types) > 1:
-        index = f"[{k+1}]"
-        print(f'{index:<4} - {"All Object Types":<24}')
-    print(f'{"[99]":<4} - {"ESC":<24}')
-    result = validate_input(types)
-    if result != -1:
-        return result
-    
-
-def get_selected_env(envs):
-    # print menu
-    q = "Which environment would you like to validate?"
-    print(q + "\n" + '=' * len(q))
-    for k, v in envs.items():
-        index = f"[{k}]"
-        print(f'{index:<4} - {v[0]:<24}')
-    if len(envs) > 1:
+    if len(the_dict) > 1:
         index = f"[{k+1}]"
         print(f'{index:<4} - {"All Object envs":<24}')
     print(f'{"[99]":<4} - {"ESC":<24}')
-    result = validate_input(envs)
+    result = validate_input(the_dict)
     if result != -1:
         return result
 
@@ -96,7 +81,6 @@ def register_job(conn, job):
     @param job: job data
     @return: job id
     """
-
     sql = ''' INSERT INTO jobs(name,metadata,content,command,destination,created)
               VALUES(?,?,?,?,?,?) '''
     cur = conn.cursor()
@@ -125,34 +109,41 @@ def generate_job(env_name, obj_type):
 
 # main
 
+config_yaml = f"{os.path.dirname(os.path.abspath(__file__))}/../config/config.yaml"
+
 # catch user CTRL+C key press
 signal(SIGINT, handler)
 
-sqlite_home = '/tmp/sqlite'
-cockpit_db = f"{sqlite_home}/cockpit.db"
+# load config yaml
+with open(config_yaml, 'r') as yf:
+    data = yaml.safe_load(yf)
+cockpit_db_home = data['params']['cockpit']['db_home']
+cockpit_db_name = data['params']['cockpit']['db_name']
+cockpit_db = f"{cockpit_db_home}/{cockpit_db_name}"
 
-cmd = f'{os.path.dirname(os.path.abspath(__file__))}/get_prd_params.py'
-response = str(subprocess.run([cmd], shell=True, stdout=subprocess.PIPE).stdout).strip('b"').split('\\n')
-print(response[0])
-exit()
+# cmd = f'{os.path.dirname(os.path.abspath(__file__))}/get_prd_params.py'
+# print(cmd)
+# response = str(subprocess.run([cmd], shell=True, stdout=subprocess.PIPE).stdout).strip('b"').split('\\n')
+# print(response[0])
+# input("press ENTER ...")
+# exit()
 
 environments = {1: ['PRD','PIVOT_PRD'], 2: ['DR', 'PIVOT_DR']}
 space_types = get_object_types()
 # choice env
-choice = get_selected_env(environments)
+choice = get_selection(environments, 'environments')
 envs = {}
 if choice == 'ALL':
     envs = environments
 else:
     envs = {1: [environments[int(choice)][0], environments[int(choice)][1]]}
 # choice type
-choice = get_selected_type(space_types)
+choice = get_selection(space_types, 'types')
 types = {}
 if choice == 'ALL':
     types = space_types
 else:
     types = {1: [space_types[int(choice)][0], space_types[int(choice)][1]]}
-
 for e in envs.values():
     the_env = e[0]
     for t in types.values():
