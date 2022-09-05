08fd699d79d6430af65921bcd0dc0aa126c059b3
segalalon <segalalon@gmail.com>
2022-09-05 13:14:51 +0300

    commented out list and show functions

--- cockpit/modules/commands.py
+++ cockpit/modules/commands.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 # *-* coding: utf-8 *-*
 
-def list_tasks():
-    print("listing tasks ...")
-    input("press ENTER to continue")
+# def list_tasks():
+#     print("listing tasks ...")
+#     input("press ENTER to continue")
 
 
-def show_task():
-    list_tasks()
-    print("showing selected task ...")
-    input("press ENTER to continue")
+# def show_task():
+#     list_tasks()
+#     print("showing selected task ...")
+#     input("press ENTER to continue")
 
 
 def validator(pivot):
