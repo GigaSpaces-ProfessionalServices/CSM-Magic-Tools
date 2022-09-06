1fa344c16a70245dbbd51cac084f6c1dd1115413
Tapan89-ux <76691807+Tapan89-ux@users.noreply.github.com>
2022-09-06 13:34:26 +0530

    Create test.sh

---
+++ alerts/telegraf/scripts/pivot/test.sh
@@ -0,0 +1,1 @@
+java -Dcom.gs.logging.debug=false -cp /dbagiga/gigaspaces-smart-ods/lib/required/*:/usr/local/bin/readFromShob-1.0.0.jar com.gigaspaces.query.PropertyValue LOCDB2T_BLL_D2TBD201_SHOB_ODS D201_ODS_TIMESTAMP mgr1,mgr2,mgr3
