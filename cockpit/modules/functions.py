36f4410d32471d45138854befeb613a91ce5b184
segalalon <segalalon@gmail.com>
2022-09-08 12:40:17 +0300

    was funcs.py - all functions from scripts

---
+++ cockpit/modules/functions.py
@@ -0,0 +1,463 @@
+#!/usr/bin/python3
+# *-* coding: utf-8 *-*
+
+
+import os
+from signal import SIGINT, signal
+import yaml
+import pyfiglet
+import subprocess
+from colorama import Fore, Style
+
+
+def handler(signal_recieved, frame):
+    '''
+    catch CTRL+C keybaord press
+    :param signal_recieved: caught by signal class
+    :param frame:
+    :return:
+    '''
+    print('\n\nOperation aborted by user!')
+    exit(0)
+
+
+def print_header():
+    '''
+    print menu header
+    '''
+    v_pref = ' ' * 2
+    version = "ODS Cockpit 2022, v1.0 | Copyright Gigaspaces Ltd"
+    #subprocess.run("clear")
+    print(pyfiglet.figlet_format("ODS Cockpit", font='slant'))
+    print(f"{v_pref}{version}\n\n")
+
+
+def pretty_print(string, color, style=None):
+    '''
+    pretty print
+    :param string: the string to pretify
+    :param color: the color to print 
+    :param style: the style to apply
+    '''
+    color = eval('Fore.' + f'{color}'.upper())
+    if style is None:
+        print(f"{color}{string}{Style.RESET_ALL}")
+    else:
+        style = eval('Style.' + f'{style}'.upper())
+        print(f"{color}{style}{string}{Style.RESET_ALL}")
+
+
+def print_locations(selections, dictionary):
+    '''
+    print locations line accordding to menu positions
+    :param selections: the selections list
+    :param dictionary: dictionary of menu items
+    :return:
+    '''
+    index = ""
+    location = f"@:: MAIN".upper()
+    for i in selections:
+        index += f"[{str(i)}]"
+        location += " :: " + str(eval(f"dictionary{index}['id']")).upper()
+    print_header()
+    pretty_print(f'{location}\n', 'green', 'bright')
+
+
+def print_menu(the_dict):
+    '''
+    print the main menu
+    :param dictionary: dictionary of menu items
+    '''
+    for k in the_dict.keys():
+        if str(k).isdigit():
+            index = f"[{k}]"
+            item = f"{the_dict[k]['id']}"
+            if the_dict[k]['description'] != '':
+                desc = f"- {the_dict[k]['description']}"
+            else:
+                desc = ""
+            print(f'{index:<4} - {item:<24}{desc:<20}')
+    print(f"{'[99]':<4} - {'ESC':<24}{'- Go Back / Exit ':<20}")
+
+
+def update_selections(the_choice, choices_list):
+    '''
+    update user selections list
+    :param the_choice: the user choice
+    :param choices_list: the choices options
+    '''
+    if the_choice == '99':
+        choices_list.pop()
+    else:
+        choices_list.append(the_choice)
+
+
+def check_settings(config):
+    '''
+    check required settings of db and network in yaml file
+    :param config: the yaml file
+    :return:
+    '''
+    db_set_required = False
+    env_set_required = False
+    # load cockpit configuration
+    with open(config, 'r') as yf:
+        data = yaml.safe_load(yf)
+
+    # get user acceptance to run
+    def get_user_permission(question):
+        q = f"{question} [yes/no]: "
+        answer = input(q).lower()
+        while True:
+            if answer == 'yes': return True
+            elif answer == 'no': return False
+            else: answer = input("invlid input! type 'yes' or 'no': ")
+
+    # cockpit database settings
+    cockpit_db_home = data['params']['cockpit']['db_home']
+    cockpit_db_name = data['params']['cockpit']['db_name']
+    cockpit_db = f"{cockpit_db_home}/{cockpit_db_name}"
+    if cockpit_db_home == '' or cockpit_db_home is None or cockpit_db_name == '' or cockpit_db_name is None:
+        pretty_print("@:: cockpit db settings".upper(), 'green', 'bright')
+        pretty_print('ERROR: cockpit.db is not set in configuration file. Aborting!', 'red')
+        exit(1)
+    elif not os.path.exists(cockpit_db):
+        db_set_required = True
+        pretty_print("@:: cockpit db settings".upper(), 'green', 'bright')
+        print("cockpit.db configuration exists but database has not been created.")
+        if get_user_permission("would you like to create the cockpit database now?"):
+            subprocess.call(['./scripts/create_db.py'], shell=True)
+            if not os.path.exists(cockpit_db): exit(1)
+        else:
+            pretty_print('ERROR: a cockpit database is required in order to run. Aborting!', 'red')
+            exit(1)
+    # cockpit enviroment settings
+    for env_name in data['params']:
+        if env_name != 'cockpit':
+            pivot = data['params'][env_name]['endpoints']['pivot']
+            if pivot == '' or pivot is None:
+                env_set_required = True
+                config_ok = False
+                break
+    if env_set_required:
+        pretty_print(f'@:: cockpit environment settings'.upper(), 'green', 'bright')
+        while not config_ok:
+            for env_name in data['params']:
+                if env_name != 'cockpit':
+                    if os.environ.get(data['params'][env_name]['variables']['pivot']) == None:
+                        errstr = "ERROR: environment variable for " + f"{env_name}".upper() + " pivot is not set. aborting!"
+                        pretty_print(errstr, 'red')
+                        exit(1)
+            pretty_print("ERROR: required parameters are not in configuration file!", 'red')
+            if get_user_permission("would you like cockpit to setup parameters automatically?"):
+                for env_name in data['params']:
+                    if env_name != 'cockpit':
+                        script = f"./scripts/get_{env_name}_params.py"
+                        subprocess.call([script], shell=True)
+                # reload cockpit configuration after changes
+                with open(config, 'r') as yf:
+                    data = yaml.safe_load(yf)
+            else:
+                print(f"\nplease set required parameters in: '{config}'\n")
+                exit()
+            config_ok = True
+            for env_name in data['params']:
+                if env_name != 'cockpit':
+                    pivot = data['params'][env_name]['endpoints']['pivot']
+                    if pivot == '' or pivot is None:
+                        config_ok = False
+                        break
+    if db_set_required or env_set_required:
+        pretty_print("\nCockpit setup and verification completed successfully.", 'green')
+        input("Press ENTER to continue to the main menu.")
+
+
+def create_database_home(db_folder):
+    '''
+    create sqlite3 home directory if doesn't exists
+    :param db_folder: sqlite3 home path
+    :return:
+    '''
+    if not os.path.exists(db_folder):
+        try:
+            os.makedirs(db_folder)
+        except OSError as e:
+            if 'Errno 13' in str(e):
+                print(f"\n{e}\n *try changing the path in config.yaml")
+            else:
+                print(e)
+            exit(1)
+
+
+def create_connection(db_file):
+    '''
+    establish a database connection (or create a new db file)
+    :param db_file: path to db file
+    :return: connection object
+    '''
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    '''
+    create a table from sql create_table_sql
+    :param conn: connection object
+    :param create_table_sql: sqlite create table statement
+    :return:
+    '''
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def get_type_selection(the_dict):
+    """
+    get object type selection from user
+    :param the_dict: menu dictionary object
+    :return: int of user choice
+    """
+    q = f"What type of task do you want to create?"
+    print(q + "\n" + '=' * len(q))
+    for k, v in the_dict.items():
+        index = f"[{k}]"
+        print(f'{index:<4} - {v:<24}')
+    print(f'{"[99]":<4} - {"ESC":<24}')
+    return int(validate_input(the_dict))
+
+
+def list_registered_jobs(conn):
+    """
+    list registered jobs in database
+    :param conn: database connection object
+    :return: list of db rows
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM jobs")
+    rows = cur.fetchall()
+    return rows
+
+
+
+def list_registered_job_names(conn):
+    '''
+    list registered job names in database
+    :param conn: connection object
+    :return: list of results
+    '''
+    c = conn.cursor()
+    c.execute("SELECT id, name FROM jobs;")
+    registered_job_names = c.fetchall()
+    return registered_job_names
+
+
+
+def list_registered_tasks(conn):
+    """
+    list registered tasks in database
+    :param conn: database connection object
+    :return: list of db rows
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks")
+    rows = cur.fetchall()
+    return rows
+
+
+def register_task(conn, task):
+    """
+    register a new task
+    :param conn: database connection object
+    :param task: task data
+    :return: task id
+    """
+    sql = ''' INSERT INTO tasks(uid,type,sn_type,job_id,metadata,content,state,created)
+              VALUES(?,?,?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+    return cur.lastrowid
+
+
+def parse_jobs_selections(jobs):
+    """
+    parse user selected jobs
+    :param jobs: list of jobs
+    :return: list of selections
+    """
+    from colorama import Fore
+    choice = input("\nEnter your choice: ")
+    
+    # check if choice in range
+    def choice_ok(value, limit):
+        if not value.isdigit() or int(value) < 1 or int(value) > limit:
+            return False
+        return True
+        
+    while True:
+        valid_selections = []
+        if choice == '99':
+            valid_selections.append(-1)
+            return valid_selections
+        else:
+            selected_ok = False
+            selected = choice.split(',')
+            for c in selected:
+                if '-' in c:
+                    range_select = c.split('-')
+                    if len(range_select) != 2:
+                        selected_ok = False
+                        break
+                    for i in range(int(range_select[0]), int(range_select[1])+1):
+                        if choice_ok(str(i), len(jobs)):
+                            selected_ok = True
+                            valid_selections.append(i)
+                        else:
+                            selected_ok = False
+                            break
+                elif choice_ok(c, len(jobs)):
+                    selected_ok = True
+                    valid_selections.append(int(c))
+                else:
+                    selected_ok = False
+                    break
+            if selected_ok:
+                return list(set(valid_selections))
+            else:
+                choice = input(f"{Fore.RED}ERROR: Invalid input!{Fore.RESET}\nEnter you choice: ")
+
+
+def discover_object_types():
+    """
+    get object types from space
+    :param [TBD]: 
+    :return: dictionary of objects 
+    """
+    types = {
+        1: ['msgPojo-1', 1001], 
+        2: ['msgPojo-2', 1002], 
+        3: ['msgPojo-3', 1003], 
+        4: ['msgPojo-4', 1004], 
+        5: ['msgPojo-5', 1005]
+        }
+    return types
+
+
+def jobs_exist(conn, new_job_name):
+    """
+    check if job exists
+    :param conn: database connection object
+    :param new_job_name: the name of job to look for 
+    :return Boolean: True/False
+    """
+    job_exists = False
+    c = conn.cursor()
+    c.execute("SELECT name FROM jobs;")
+    registered_job_names = c.fetchall()
+    for name in registered_job_names:
+        if new_job_name in name:
+            job_exists = True
+            break
+    return job_exists
+
+
+def register_job(conn, job):
+    """
+    register a new job
+    :param conn: database connection object
+    :param job: job data
+    :return: job id
+    """
+    sql = ''' INSERT INTO jobs(name,metadata,content,command,destination,created)
+              VALUES(?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, job)
+    conn.commit()
+    return cur.lastrowid
+
+
+def generate_job_file(env_name, obj_type, yaml_data):
+    """
+    create a file for a job
+    :param env_name: name of environment
+    :param obj_type: name of target object
+    :param yaml_data: data from config yaml
+    :return:
+    """
+    env_name_low = env_name.lower()
+    pivot = f"PIVOT_{env_name}"
+    jobs_home = f"{os.path.dirname(os.path.abspath(__file__))}/../jobs"
+    job_file_name = f"validation_{env_name}_{obj_type}.py".lower()
+    job_file = f"{jobs_home}/{job_file_name}"
+    pivot = yaml_data['params'][env_name_low]['endpoints']['pivot']
+    cmd = "cat {exec_script} | ssh " + pivot + " python3 -"
+    sp_exec = 'subprocess.run([cmd], shell=True, stdout=subprocess.PIPE).stdout.decode()'
+    lines = [
+        '#!/usr/bin/python3\n\n',
+        'import subprocess\n',
+        f'exec_script = "{os.path.dirname(os.path.abspath(__file__))}/get_space_objects.py"',
+        f'cmd = f"{cmd}"',
+        f'response = {sp_exec}',
+        f'print(response)\n\n'
+    ]
+    # create jobs home folder if not exists
+    if not os.path.exists(jobs_home):
+        try:
+            os.makedirs(jobs_home)
+        except OSError as e:
+            print(e)
+    with open(job_file, 'w') as j:
+        j.writelines('\n'.join(lines))
+    # set execution bit for job file
+    subprocess.run([f"chmod +x {job_file}"], shell=True)
+
+
+def list_db_tables(conn):
+    """
+    list tables in database
+    :param conn: database connection object
+    :return:
+    """    
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = c.fetchall()
+    if len(tables) > 0:
+        print("\n[Cockpit database tables]")
+        for table_name in tables:
+            c.execute(f"SELECT count(*) FROM {table_name[0]};")
+            num = c.fetchall()[0][0]
+            num_records = f"{num} record(s)"
+            print(f"   {table_name[0]:<10} : {num_records:<10}")
+        c.close()
+    conn.close()
+
+
+def list_policies(conn):
+    '''
+    get the list of policies from database
+    :param conn: connection object
+    :return: list of results
+    '''
+    c = conn.cursor()
+    c.execute("SELECT * FROM policies;")
+    registered_policies = c.fetchall()
+    return registered_policies
+
+
+def list_tasks(conn):
+    '''
+    get the list of tasks from database
+    :param conn: connection object
+    :return: list of results
+    '''    
+    c = conn.cursor()
+    c.execute("SELECT id, uid, type FROM tasks;")
+    registered_tasks = c.fetchall()
+    return registered_tasks
+
