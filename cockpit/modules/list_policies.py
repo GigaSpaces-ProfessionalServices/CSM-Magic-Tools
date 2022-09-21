24337a225842a9c928173ae5002099e49683b2aa
segalalon <segalalon@gmail.com>
2022-09-21 14:27:44 +0300

    changed path construct + table print put

--- cockpit/modules/list_policies.py
+++ cockpit/modules/list_policies.py
@@ -11,7 +11,7 @@ from functions import handler, create_connection, list_policies
 
 
 # main
-config_yaml = f"{os.path.dirname(os.path.realpath(__file__))}/../config/config.yaml"
+config_yaml = f"{os.environ['COCKPIT_HOME']}/config/config.yaml"
 
 # catch user CTRL+C key press
 signal(SIGINT, handler)
@@ -25,18 +25,17 @@ cockpit_db = f"{cockpit_db_home}/{cockpit_db_name}"
 conn = create_connection(cockpit_db)
 policies = list_policies(conn,'')
 if len(policies) > 0:
-    w = 93
-    print("-" * w)
-    print(f'| {"Id":<4} | {"Schedule":<10} | {"Repeat Times":<12}| {"Repeat Every":<12} | {"Associated Task UID":<40} |')    
-    print("-" * w)
+    spc = '- -'
+    print(f'{"Id":<4} | {"Name":<30} | {"UID":<38} | {"Schedule":<8} | {"Associated Task":<38} | {"Is_Active":<11}')
+    print("-"*4 + spc + "-"*30 + spc + "-"*38 + spc + "-"*8 + spc + "-"*38 + spc + "-"*11)
     for policy in policies:
         p_id = policy[0]
-        p_sched = f"{policy[2]} sec"
-        p_repeat = f"{policy[3]}"
-        p_wait = f"{policy[4]} sec"
-        p_assoc_task = policy[5]
-        print(f'| {p_id:<4} | {p_sched:<10} | {p_repeat:<12}| {p_wait:<12} | {p_assoc_task:<40} |')
-    print("-"*w)
+        p_uid = policy[1]
+        p_name = f"{policy[2]}"
+        p_sched = f"{policy[3]} sec"
+        p_assoc_task = policy[4]
+        p_active = f"{policy[7]}"
+        print(f'{p_id:<4} | {p_name:<30} | {p_uid:<38} | {p_sched:<8} | {p_assoc_task:<38} | {p_active:<11}')
 else:
     print("No policies found")
 
