13961a459325c743302f675ad946aa66e5ebe0d0
segalalon <segalalon@gmail.com>
2022-09-21 16:05:23 +0300

    changed: set COCKPIT_HOME as system wide variable

--- cockpit/set_env.sh
+++ cockpit/set_env.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 #
-# setting COCKPIT_HOME value in bashrc
+# setting COCKPIT_HOME value in /etc/environment
 #
 
+ENV_FILE=/etc/environment
 COCKPIT_HOME=$(realpath $(dirname $0))
 
 # remove current setting
-sed -i '/COCKPIT_HOME=/d' ~/.bashrc
+sed -i '/COCKPIT_HOME=/d' $ENV_FILE
 
 # insert new setting
-echo "export COCKPIT_HOME=${COCKPIT_HOME}" >> ~/.bashrc
+echo "export COCKPIT_HOME=${COCKPIT_HOME}" >> $ENV_FILE
 
 echo "set COCKPIT_HOME to: '${COCKPIT_HOME}'"
 
 # source it
-source ~/.bashrc && exec bash
+source $ENV_FILE && exec bash
