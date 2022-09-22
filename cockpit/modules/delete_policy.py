4d9bcff184ab0b9102394bcec438b3c3b94b8084
segalalon <segalalon@gmail.com>
2022-09-22 12:40:40 +0300

    improved logic for delete demo

--- cockpit/modules/delete_policy.py
+++ cockpit/modules/delete_policy.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 # *-* coding: utf-8 *-*
 
+from operator import index
 import os
 import yaml
 from signal import SIGINT, signal
-from functions import create_connection, handler
+from functions import create_connection, get_selection, \
+    handler, list_policies
 
 # main
-config_yaml = f"{os.environ['COCKPIT_HOME']}/../config/config.yaml"
+config_yaml = f"{os.environ['COCKPIT_HOME']}/config/config.yaml"
 
 # catch user CTRL+C key press
 signal(SIGINT, handler)
@@ -20,7 +22,25 @@ cockpit_db_name = data['params']['cockpit']['db_name']
 cockpit_db = f"{cockpit_db_home}/{cockpit_db_name}"
 conn = create_connection(cockpit_db)
 
-print("### [ TBD ] ### DELETE POLICY ###")
+# list policies
+policies = {}
+index = 1
+for pol in list_policies(conn, ''):
+    policies.update({index:[f'{pol[2]}']})
+    index += 1
+if len(policies) > 0:
+    # choice policy
+    q = f"Which policy would you like to delete?"
+    choice = get_selection(policies, 'Policies', q)
+    if choice == 'ALL':
+        pol_selected = policies
+    else:
+        pol_selected = {1: [policies[int(choice)][0]]}
+else:
+    print("No policies found")
+
+print(f"\n[DEMO]\nCHOSE TO DELETE POLICY: {pol_selected[int(choice)][0]}")
+
 
     # TO REMOVE
     # systemctl stop cockpit_policy_60.timer 
