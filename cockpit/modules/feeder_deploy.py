ee751d08f3a48d4a13a26537bee4f9267c58827a
segalalon <segalalon@gmail.com>
2022-09-22 10:48:08 +0300

    added error print + mockup printing for demo

--- cockpit/modules/feeder_deploy.py
+++ cockpit/modules/feeder_deploy.py
@@ -9,22 +9,8 @@ from signal import SIGINT, signal
 import subprocess
 import json
 from functions import create_connection, handler, \
-    check_connection, validate_input
-
-def get_selection(the_dict, description):
-    # print menu
-    print(description + "\n" + '=' * len(description))
-    for k, v in the_dict.items():
-        index = f"[{k}]"
-        print(f'{index:<4} - {v[0]:<24}')
-    if len(the_dict) > 1:
-        index = f"[{k+1}]"
-        item = "All"
-        print(f'{index:<4} - {item:<24}')
-    print(f'{"[99]":<4} - {"ESC":<24}')
-    result = validate_input(the_dict)
-    if result != -1:
-        return result
+    check_connection, validate_input, get_selection, \
+        pretty_print
 
 
 # main
@@ -50,39 +36,44 @@ for k, v in data['params'].items():
         index += 1
 
 # choice env
-choice = get_selection(environments, "Where would you like to deploy the feeder?")
+q = "Where would you like to deploy the feeder?"
+choice = get_selection(environments, 'Environments', q)
 envs = {}
 if choice == 'ALL':
-    envs = environments
+    env_select = environments
 else:
-    envs = {1: [environments[int(choice)][0], environments[int(choice)][1]]}
+    env_select = {1: [environments[int(choice)][0], environments[int(choice)][1]]}
 
 # generate feeders dictionary
 feeders = {}
 for k, v in data['feeders'].items(): feeders[k] = [v['name']]
 
 # choice feeder
-choice = get_selection(feeders, "Which feeder would you like to deploy?")
-feed_select = {}
+q = "Which feeder would you like to deploy?"
+choice = get_selection(feeders, 'Feeders', q)
+feeder_select = {}
 if choice == 'ALL':
-    feed_select = feeders
+    feeder_select = feeders
 else:
-    feed_select = {1: [feeders[int(choice)][0], feeders[int(choice)][1]]}
+    feeder_select = {1: [feeders[int(choice)][0]]}
 
-# run deployments
-connections_ok = []
-for e in environments.values():
+# run feeder deployments
+for e in env_select.values():
     env_name = e[0].lower()
     pivot = data['params'][env_name]['endpoints']['pivot']
-    #exec_script = f"{os.path.dirname(os.path.realpath(__file__))}/get_space_objects.py"
-    if check_connection(pivot, 22):
-        for f in feeders.values():
+    port = 22
+    if check_connection(pivot, port):
+        for f in feeder_select.values():
             feeder_name = f[0].lower()
-            cmd = f'ssh {pivot} "/dbagiga/josh/auto_odsx/auto_{feeder_name}feederdeploy"'
-            response = subprocess.run([cmd], shell=True, stdout=subprocess.PIPE).stdout.decode()
+            print(f"\nDeploying '{feeder_name} feeder' on {env_name}")
+            #cmd = f'ssh {pivot} "/dbagiga/josh/auto_odsx/auto_{feeder_name}feederdeploy"'
+            #response = subprocess.run([cmd], shell=True, stdout=subprocess.PIPE).stdout.decode()
             #response = json.loads(response.replace("\'", "\""))
-            ###########################################################################
+            #
+            #
             ### NEED TO ADD A ROUTINE TO CHECK THAT FEEDER DEPLOYMENT IS SUCCESSFUL ###
-            ###########################################################################
-
+            #
+            #
+    else:
+        pretty_print(f"ERROR: connection to {e[0]} pivot ({pivot}:{port}) could not be established", 'red')
 input("\nPress ENTER to go back to the main menu")
