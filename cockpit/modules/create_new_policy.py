d43d7d84c8e0dbff68158a43f1748bf2f7bc1e9e
segalalon <segalalon@gmail.com>
2022-10-02 12:25:24 +0300

    added logic + new generic db functions

--- cockpit/modules/create_new_policy.py
+++ cockpit/modules/create_new_policy.py
@@ -2,14 +2,88 @@
 # *-* coding: utf-8 *-*
 
 import os
+from secrets import choice
 import yaml
 import datetime
 import subprocess
 import uuid
+from signal import signal, SIGINT
 from colorama import Fore, Style
-from functions import create_connection, list_tasks_grouped, get_user_ok, \
-    validate_option_select, pretty_print, register_policy, sort_tuples_list, \
-        create_file, list_jobs_by_task_uid, policy_schedule_exists, get_keypress
+from functions import (
+    create_connection,
+    db_insert,
+    db_select, 
+    get_user_ok,
+    press_any_key, 
+    validate_option_select, 
+    pretty_print, 
+    sort_tuples_list, 
+    create_file,  
+    get_keypress
+    )
+
+def deploy_systemd_components():
+    # generate policy manager script from template
+    if not os.path.exists(f"{policies_home}/{policy_manager_script}"):
+        template = f"{templates_dir}/policy_manager_template.py"
+        copy_template = f"cp {template} {policies_home}/{policy_manager_script}"
+        try:
+            subprocess.run([copy_template], shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+        else:
+            try:    # set execution bit for policy manager script
+                subprocess.run([f"chmod +x {policies_home}/{policy_manager_script}"], shell=True, check=True)
+            except subprocess.CalledProcessError as e:
+                print(e.output)
+            print(f"policy manager script '{policy_manager_script}'{Fore.GREEN}created successfully!{Style.RESET_ALL}")
+    else:
+        print(f"policy manager script already exists. {Fore.RED}creation aborted!{Style.RESET_ALL}")
+    
+    # create systemd policy.service
+    lines = [
+        '[Unit]',
+        f'Description={policy_desc}',
+        f'Wants={suffix}_{policy_name}.timer\n',
+        '[Service]',
+        f'Environment="COCKPIT_HOME={os.environ["COCKPIT_HOME"]}"',
+        'Type=oneshot',
+        f'ExecStart={os.path.realpath(policies_home)}/{policy_manager_script}\n',
+        '[Install]',
+        'WantedBy=multi-user.target\n'
+        ]
+    create_file(lines, policy_service)
+    
+    # create systemd policy.timer    
+    lines = [
+        '[Unit]',
+        f'Description={policy_desc}',
+        f'Requires={suffix}_{policy_name}.service\n',
+        '[Timer]',
+        f'Unit={suffix}_{policy_name}',
+        'AccuracySec=1us',
+        f'OnUnitActiveSec={schedule}\n',
+        '[Install]',
+        'WantedBy=timers.target\n'
+        ]
+    create_file(lines, policy_timer)
+    
+    # reload daemons
+    subprocess.run(["systemctl daemon-reload"], shell=True)
+    
+    # enable timer service if needed
+    cur = conn.cursor()
+    sql = f"SELECT active_state FROM policies where name = ? GROUP BY name;"
+    cur.execute(sql, (policy_name,))
+    rows = cur.fetchall()
+    if rows[0][0] == 'yes':
+        try:
+            subprocess.run([f"systemctl enable --now {policy_timer}"], shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+    
+    print("CREATE POLICY MANAGER SUCCESSFULLY!")
+    print(f"{'Schedule:':<12}\n   run every {str(sched_min)}m:{str(sched_sec)}s")
 
 # main
 config_yaml = f"{os.environ['COCKPIT_HOME']}/config/config.yaml"
@@ -29,19 +103,19 @@ conn = create_connection(cockpit_db)
 
 # introduction
 intro = [
-    "[ INTRO ]",
-    "A policy is comprised of two elements (i.e: files and/or database records):",
-    " - Policy manager: has corresponding systemd services and is unique for each schedule defined.",
+    "A policy is defined by two types of files:",
+    " - Policy manager: activated by systemd services and is unique for each schedule defined.",
     " - Policy worker: is the element created for each task(s) associated with a specific schedule.",
-    " * A single manager can run multiple workers"
+    "(!) A single manager can run multiple workers"
     ]
 for line in intro: pretty_print(line, 'LIGHTBLUE_EX')
 
 # get task(s) from database
-tasks = sort_tuples_list(list_tasks_grouped(conn, 'id', 'uid', 'type'))
+sql = f"SELECT id, uid, type FROM tasks GROUP BY uid"
+tasks = sort_tuples_list(db_select(conn, sql))
 if len(tasks) == 0:
     pretty_print("\nERROR: tasks must be created first to add new policies!", 'red')
-    input("\nPress ENTER to go back to the menu")
+    press_any_key()
     exit()
 
 # create policies home if not exist
@@ -54,73 +128,106 @@ if not os.path.exists(policies_home):
 
 # get policy schedule
 schedule_exists = False
-note = "Choose a schedule (in seconds) for this policy"
-print(f"\n{note}\n" + '* Example: 80 means "run every 1m:20s"\n' + '-'*len(note))
-while True:
-    schedule = input("Enter schedule: ")
-    if not schedule.isdigit():
-        print(f"{Fore.RED}ERROR: Input must be a number!{Fore.RESET}")
-        continue
-    else:
-        schedule = int(schedule)
-        break
+title = "Enter a schedule (in seconds) for this policy"
+note = '* Example: 80 means "run every 1m:20s"'
+print(f"\n{title}\n{note}\n" + '-'*len(title))
+try:
+    while True:
+        schedule = get_keypress()
+        if schedule == 'esc':
+            quit()
+        if not schedule.isdigit():
+            pretty_print('ERROR: Input must be a number!', 'red')
+            continue
+        else:
+            schedule = int(schedule)
+            break
+except (KeyboardInterrupt, SystemExit):
+    os.system('stty sane')
+    quit()
 sched_sec = schedule % 60
 sched_min = int(schedule / 60)
-if policy_schedule_exists(conn, schedule):
+sql = f"SELECT schedule_sec FROM policies WHERE schedule_sec = '{schedule}' GROUP BY schedule_sec;"
+if len(db_select(conn, sql)) > 0:
     schedule_exists = True
-    pretty_print("(!) a manager already exists for this schedule. only workers will be added!", 'LIGHTYELLOW_EX')
+    pretty_print("(!) a policy already exists for this schedule. only workers will be added!", 'LIGHTYELLOW_EX')
+print('\n')
 
 # get policy name
-p_name_len = 30
-note = f"Choose a name for this policy (max {p_name_len} chars) or leave blank to auto-generate"
-print(f"\n{note}\n"  + '-'*len(note))
-name_ok = False
-while not name_ok:
-    p_name = input("Enter name: ")
-    if len(p_name) <= 30:
-        name_ok = True
-        if len(p_name) == 0:
-            p_name = ""
-    else:
-        pretty_print(f"(!) policy name cannot exceed {p_name_len} characters", 'red')
-if p_name == "":
+if not schedule_exists:
+    p_name_len = 30
+    title = f"Enter a name for this policy or press ENTER to auto-generate"
+    note = f"(!) max {p_name_len} characters allowed"
+    print(f"\n{title}\n{note}\n" + '-'*len(title))
+    name_ok = False
+    try:
+        while not name_ok:
+            p_name = get_keypress()
+            if p_name == 'esc':
+                quit()
+            if len(p_name) <= 30:
+                name_ok = True
+                if len(p_name) == 0:
+                    p_name = ""
+            else:
+                pretty_print(f"(!) policy name cannot exceed {p_name_len} characters", 'red')
+            if p_name == "":
+                policy_name = f"policy_{schedule}"
+                auto_gen_name = True
+            else:
+                policy_name = p_name
+                auto_gen_name = False
+    except (KeyboardInterrupt, SystemExit):
+        os.system('stty sane')
+        quit()
+    if auto_gen_name:
+        print("* auto generated\n\n")
+else:
     policy_name = f"policy_{schedule}"
     auto_gen_name = True
-else:
-    policy_name = p_name
-    auto_gen_name = False
 
 # get task(s) selection
 selected_tasks = []
 index = 1
 tasks_dict = {}
+tasks_menu_dict = {}
 for task in tasks:
+    task_id = task[0]
     task_uid = task[1]
     task_type = task[2]
-    if task_type == data['tasks'][1]['name']:
+    if task_type.lower() == 'Validation'.lower():
         # we extract the target object type from the task
-        task_obj = ''.join(set([job[4] for job in list_jobs_by_task_uid(conn, (task_uid,))]))
-    # conform tasks dictionary to validation func
-    tasks_dict[task[0]] = [f"{task_type} of object type '{task_obj}'", task_uid]
-title = "Choose tasks to be associated with this policy"
-for i in validate_option_select(tasks_dict, title):
-    if i != -1:
-        selected_tasks.append(i-1)
+        sql = f""" SELECT j.name, j.id, j.destination, j.metadata, j.content 
+              FROM tasks t INNER JOIN jobs j 
+              ON j.id = t.job_id 
+              WHERE t.uid = '{task_uid}'; """
+        task_obj = ''.join(set([job[4] for job in db_select(conn, sql)]))
+        # conform tasks dictionary to validation func
+        tasks_menu_dict[task_id] = [f"{task_type} of object type '{task_obj}'", task_uid]
+        tasks_dict[task_id] = [task_type, task_uid]
     else:
-        pretty_print("\n(!) no tasks have been selected. cannot continue!", 'red')
-        input("\nPress ENTER to go back to the menu")
-        exit()
+        print("TBD - add procedures for tasks other than validation...")
+title = "Choose tasks to be associated with this policy"
+selected_tasks = validate_option_select(tasks_menu_dict, title)
+if selected_tasks == None: quit()
+if -1 in selected_tasks:
+    pretty_print("\n(!) no tasks have been selected. cannot continue!", 'red')
+    press_any_key()
+    exit()
 
 # confirm policy parameters
-pretty_print("\n\n(!) Please confirm the following policy creation:", 'yellow')
-if auto_gen_name: print(f"   {'Name:':<12}{policy_name} (name auto generated)")
-else: print(f"   {'Name:':<18}{policy_name}")
+pretty_print("\n\n(!) Please confirm the following policy parameters:", 'yellow')
+if schedule_exists:
+    print(f"   {'Name:':<18}{policy_name} - already exists!")
+else:
+    if auto_gen_name: print(f"   {'Name:':<18}{policy_name} (name auto generated)")
+    else: print(f"   {'Name:':<18}{policy_name}")
 print(f"   {'Run every:':<18}{str(sched_min)}m:{str(sched_sec)}s")
 print(f"   {'Associated Tasks:':<18}")
 for t in selected_tasks:
-    task_uid = tasks[t][1]
-    task_type = tasks[t][2]
-    print(f"   uid: {task_uid:<38}, type: {task_type}")
+    task_type = tasks_dict[t][0]
+    task_uid = tasks_dict[t][1]
+    print(f"{' ':<6}Id: {t}, type: {task_type}, uid: {task_uid:<38}")
 
 if get_user_ok("\nContinue with policy registration?"):
     suffix = 'cockpit'
@@ -132,81 +239,25 @@ if get_user_ok("\nContinue with policy registration?"):
     print("\n")
     
     # if the schedule is new we create the policy manager and services
-    if not schedule_exists:
-        # generate policy manager script from template
-        if not os.path.exists(f"{policies_home}/{policy_manager_script}"):
-            template = f"{templates_dir}/policy_manager_template.py"
-            copy_template = f"cp {template} {policies_home}/{policy_manager_script}"
-            try:
-                subprocess.run([copy_template], shell=True, check=True)
-            except subprocess.CalledProcessError as e:
-                print(e.output)
-            else:
-                try:    # set execution bit for policy init script
-                    subprocess.run([f"chmod +x {policies_home}/{policy_manager_script}"], shell=True, check=True)
-                except subprocess.CalledProcessError as e:
-                    print(e.output)
-                print(f"policy manager script '{policy_manager_script}'{Fore.GREEN}created successfully!{Style.RESET_ALL}")
-        else:
-            print(f"policy manager script already exists. {Fore.RED}creation aborted!{Style.RESET_ALL}")
-        
-        # create systemd policy.service
-        lines = [
-            '[Unit]',
-            f'Description={policy_desc}',
-            f'Wants={suffix}_{policy_name}.timer\n',
-            '[Service]',
-            f'Environment="COCKPIT_HOME={os.environ["COCKPIT_HOME"]}"',
-            'Type=oneshot',
-            f'ExecStart={os.path.realpath(policies_home)}/{policy_manager_script}\n',
-            '[Install]',
-            'WantedBy=multi-user.target\n'
-            ]
-        create_file(lines, policy_service)
-        
-        # create systemd policy.timer    
-        lines = [
-            '[Unit]',
-            f'Description={policy_desc}',
-            f'Requires={suffix}_{policy_name}.service\n',
-            '[Timer]',
-            f'Unit={suffix}_{policy_name}',
-            'AccuracySec=1us',
-            f'OnUnitActiveSec={schedule}\n',
-            '[Install]',
-            'WantedBy=timers.target\n'
-            ]
-        create_file(lines, policy_timer)
-        
-        # reload daemons
-        subprocess.run(["systemctl daemon-reload"], shell=True)
-        
-        # enable timer service if needed
-        cur = conn.cursor()
-        sql = f"SELECT active_state FROM policies where name = ? GROUP BY name;"
-        cur.execute(sql, (policy_name,))
-        rows = cur.fetchall()
-        if rows[0][0] == 'yes':
-            try:
-                subprocess.run([f"systemctl enable --now {policy_timer}"], shell=True, check=True)
-            except subprocess.CalledProcessError as e:
-                print(e.output)
-        
-        print("CREATE POLICY MANAGER SUCCESSFULLY!")
-        print(f"{'Schedule:':<12}\n   run every {str(sched_min)}m:{str(sched_sec)}s")
+    if not schedule_exists: deploy_systemd_components()
+    
     # register and generate policy worker(s)
-    for task in selected_tasks:     
-        task_uid = tasks[task][1]
-        task_type = tasks[task][2]
+    for task_id in selected_tasks:
+        task_type = tasks_dict[task_id][0]
+        task_uid = tasks_dict[task_id][1]
         pol_uid = str(uuid.uuid4())
         policy_worker_script = f"{pol_uid}.py"
         p_metadata = 'NULL'
         p_content = 'NULL'
         p_created = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        p_state = 'yes'
+        p_active = 'yes'
+        
         # register worker in database
-        policy_data = (pol_uid,policy_name,schedule,task_uid,p_metadata,p_content,p_state,p_created)
-        r = register_policy(conn, policy_data)
+        sql = """ INSERT INTO policies(uid, name, schedule_sec, task_id, task_uid, metadata, content, active_state, created) 
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?) """
+        policy_data = (pol_uid, policy_name, schedule, task_id, task_uid, p_metadata, p_content, p_active, p_created)
+        r = db_insert(conn, sql, policy_data)
+        
         # generate policy worker script from template
         if not os.path.exists(f"{policies_workers_home}/{policy_worker_script}"):
             template = f"{templates_dir}/policy_worker_template.py"
@@ -220,14 +271,11 @@ if get_user_ok("\nContinue with policy registration?"):
                     subprocess.run([f"chmod +x {policies_workers_home}/{policy_worker_script}"], shell=True, check=True)
                 except subprocess.CalledProcessError as e:
                     print(e.output)
-                print(f"policy worker script '{policy_worker_script}'{Fore.GREEN}created successfully!{Style.RESET_ALL}")
+                finally:    # print summary
+                    print(f"\nPolicy worker '{pol_uid}' {Fore.GREEN}created successfully!{Style.RESET_ALL}")
+                    print("Associated Tasks:")
+                    print(f"   uid: {task_uid:<38}, type: {task_type}")
         else:
-            print(f"policy worker script already exists. {Fore.RED}creation aborted!{Style.RESET_ALL}")    
-        # print summary
-        print(f"\nPolicy worker '{pol_uid}' {Fore.GREEN}created successfully!{Style.RESET_ALL}")
-        print("Associated Tasks:")
-        print(f"   uid: {task_uid:<38}, type: {task_type}")
-    print()    
-else:
-    print(f"\nPolicy {Fore.RED}creation aborted by user!{Style.RESET_ALL}")
-input("\nPress ENTER to go back to the menu")
+            print(f"policy worker script already exists. {Fore.RED}creation aborted!{Style.RESET_ALL}")
+    
+    press_any_key()
