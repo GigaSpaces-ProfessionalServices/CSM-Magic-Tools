a2e76e261c813ad8d493de33cdc9e4bc5d417098
segalalon <segalalon@gmail.com>
2022-10-03 13:47:04 +0300

    added multiprocessing to task executions

--- cockpit/templates/policy_worker_template.py
+++ cockpit/templates/policy_worker_template.py
@@ -4,7 +4,11 @@
 import os
 import yaml
 import json
+import random
 import subprocess
+from time import sleep
+import datetime
+import multiprocessing
 
 def create_connection(db_file):
     """
@@ -72,42 +76,90 @@ def write_to_influx(dbname, data):
     client.write_points(json_body)
 
 
-# main
-os.environ['COCKPIT_HOME']
-config_yaml = f"{os.environ['COCKPIT_HOME']}/config/config.yaml"
-policies_home = f"{os.environ['COCKPIT_HOME']}/policies"
-policies_exec_home = f"{policies_home}/exec"
-jobs_home = f"{os.environ['COCKPIT_HOME']}/jobs"
+def calculate_retries(conn, puid):
+    """
+    calculate retry cycles and intervals accordding to database setting
+    :param conn: database connection object
+    :return: dictionary of {retry, wait}
+    """
+    sql = f"SELECT schedule_sec, retry FROM policies WHERE uid = '{puid}'"
+    _sch, _rtr = db_select(conn, sql)[0]
+    _wait = int(_sch / (_rtr + 1))   # the interval between retries
+    return {'retry': _rtr, 'wait': _wait}
 
-# get policy schedule from file name
-policy_uid = '.'.join(os.path.basename(__file__).split('.')[:-1])
 
-# load config yaml
-with open(config_yaml, 'r') as yf:
-    data = yaml.safe_load(yf)
+def exec_task_routine(_retry, _wait):
+    
+    # alert handeling
+    def raise_alert(_string):
+        print(_string)  # DEBUG
 
-cockpit_db_home = data['params']['cockpit']['db_home']
-cockpit_db_name = data['params']['cockpit']['db_name']
-cockpit_db = f"{cockpit_db_home}/{cockpit_db_name}"
-conn = create_connection(cockpit_db)
-sql = f"SELECT task_uid FROM policies WHERE uid = '{policy_uid}';"
-for task_uid in db_select(conn, sql):
-    print(f"\n[ Task {task_uid[0]} ]")
-    sql = f""" SELECT j.name, j.id, j.destination, j.metadata, j.content 
-            FROM tasks t INNER JOIN jobs j 
-            ON j.id = t.job_id 
-            WHERE t.uid = '{task_uid}'; """
-    for job in db_select(conn, sql):
-        job_file = f"{job[0]}.py".lower()
-        job_dest_env= job[2]
-        job_type = job[3]
-        script = f'{jobs_home}/{job_file}'
-        print(f"   Executing job: {job_file}")
-        if job_type == 'counter':
+    i = 1
+    results_ok = False
+    while i < _retry + 1:
+        print(f"\n[ Task {task_uid[0]}: run # {i} ]")
+        sql = f""" SELECT j.name, j.id, j.destination, j.metadata, j.content 
+                FROM tasks t INNER JOIN jobs j 
+                ON j.id = t.job_id 
+                WHERE t.uid = '{task_uid}'; """
+        job_results = []
+        for job in db_select(conn, sql):
+            job_file = f"{job[0]}.py".lower()
+            job_dest_env= job[2]
+            job_type = job[3]
             job_obj_type = job[4]
-            response = subprocess.run([script], shell=True, stdout=subprocess.PIPE).stdout.decode()
-            response = json.loads(response.replace("\'", "\""))   # converting string to dictionary
-            for k,v in response.items():
-                if k == job_obj_type:
-                    influx_data = {'env': job_dest_env, 'obj_type': job_obj_type, 'count': v['entries']}
-        write_to_influx('mydb', influx_data)
+            script = f'{jobs_home}/{job_file}'
+            print(f"   Executing job: {job_file}")
+            if job_type == 'counter':
+                response = subprocess.run([script], shell=True, stdout=subprocess.PIPE).stdout.decode()
+                response = json.loads(response.replace("\'", "\""))   # converting string to dictionary
+                for k,v in response.items():
+                    if k == job_obj_type:
+                        ### DEBUG ###
+                        job_results.append(random.randrange(100,101))
+                        print(f"RECORDED {job_obj_type} ENTRIES INTO JOB_RESULTS => {job_results}")
+
+                        #job_results.append(int(v['entries']))
+                        influx_data = {'env': job_dest_env, 'obj_type': job_obj_type, 'count': v['entries']}
+            write_to_influx('mydb', influx_data)
+            # check if results vary to raise an alert    
+            if job_results.count(job_results[0]) == len(job_results):
+                print("RESULTS MATCH!")
+                results_ok = True
+                break
+        if results_ok: break
+        i += 1
+        if i < _retry + 1: sleep(_wait)
+        
+    # if results not ok we raise an alert
+    if not results_ok:    
+        raise_alert(f"{datetime.datetime.now()} [FAILED] object type: {job_obj_type}")
+
+
+
+if __name__ == '__main__':
+    os.environ['COCKPIT_HOME']
+    config_yaml = f"{os.environ['COCKPIT_HOME']}/config/config.yaml"
+    policies_home = f"{os.environ['COCKPIT_HOME']}/policies"
+    policies_exec_home = f"{policies_home}/exec"
+    jobs_home = f"{os.environ['COCKPIT_HOME']}/jobs"
+
+    # get policy schedule from file name
+    policy_uid = '.'.join(os.path.basename(__file__).split('.')[:-1])
+
+    # load config yaml
+    with open(config_yaml, 'r') as yf:
+        data = yaml.safe_load(yf)
+
+    cockpit_db_home = data['params']['cockpit']['db_home']
+    cockpit_db_name = data['params']['cockpit']['db_name']
+    cockpit_db = f"{cockpit_db_home}/{cockpit_db_name}"
+    conn = create_connection(cockpit_db)
+
+    # get retry and wait params
+    for _retry, _wait in calculate_retries(conn, policy_uid): pass
+    rand_wait = _wait + random.randrange(-2, 2)  # we randomize wait time by offset of 2
+    multiprocessing.set_start_method('spawn')
+    sql = f"SELECT task_uid FROM policies WHERE uid = '{policy_uid}';"
+    for task_uid in db_select(conn, sql):
+        multiprocessing.Process(target=exec_task_routine, args=(_retry, rand_wait)).start()
