03f36fab4dc0eed42063bd35a89915682c3b895f
segalalon <segalalon@gmail.com>
2022-10-03 14:26:26 +0300

    renamed

---
+++ cockpit/utils/purge_cockpit_policies.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#
+# purge cockpit policies (cockpit services and scripts)
+#
+
+SYS_HOME=/etc/systemd/system
+COCKPIT_HOME=$(realpath $(dirname $0))
+
+echo "WARNING!!!"
+echo "THIS WILL DELETE ANY COCKPIT POLICIES ON THE SYSTEM AND MIGHT BREAK COCKPIT OPERATIONS"
+echo "OPERATIONS INCLUDE:"
+echo " - SYSTEMD TIMERS AND SERVICES DELETION"
+echo " - POLICY MANGER AND WORKER FILES DELETION"
+echo
+echo "*** RELATED DATABASE RECORDS WILL HAVE TO BE REMOVED MANUALLY ***"
+
+read -p "Do you wish to continue? [yes/no]   " ans
+if [[ ${ans^^} == "YES" ]]; then
+    for unit in $(systemctl list-timers | awk '{print $13}' | grep cockpit); do
+        p_name=${unit%.*}
+        p_service=${p_name}.service
+        # stop
+        systemctl stop ${unit}
+        systemctl stop ${p_service}
+        # disable
+        systemctl disable ${unit}
+        systemctl disable ${p_service}
+        # remove
+        rm -f ${SYS_HOME}/${policy_name}*
+        # reload
+        systemctl daemon-reload
+    done
+
+    # delete policy files
+    find ${COCKPIT_HOME}/policies -name "${policy_name}*" -exec rm -f {} \;
+fi
+
+exit
