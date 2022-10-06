21479aee2845a7c8e2c721afa7bdcb7dfba7d3d8
segalalon <segalalon@gmail.com>
2022-10-06 15:01:53 +0300

    minor print change

--- cockpit/utils/set_env.sh
+++ cockpit/utils/set_env.sh
@@ -13,7 +13,7 @@ sed -i '/COCKPIT_HOME=/d' $ENV_FILE
 # insert new setting
 echo "export COCKPIT_HOME=${COCKPIT_HOME}" >> $ENV_FILE
 
-echo "set COCKPIT_HOME to: '${COCKPIT_HOME}'"
+echo "COCKPIT_HOME has been set to: '${COCKPIT_HOME}'"
 
 # source it
 source $ENV_FILE && exec bash
