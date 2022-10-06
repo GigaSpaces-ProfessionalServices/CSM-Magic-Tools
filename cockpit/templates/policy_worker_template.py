873b8e6b15cca3ff445d1f1a7fa5d03481609f6e
segalalon <segalalon@gmail.com>
2022-10-06 17:22:19 +0300

    fixed task basic multiprocessing

--- cockpit/templates/policy_worker_template.py
+++ cockpit/templates/policy_worker_template.py
@@ -4,7 +4,7 @@
 import os
 import yaml
 import json
-import random
+from random import randrange
 import subprocess
 from time import sleep
 import datetime
@@ -79,15 +79,15 @@ def calculate_retries(conn, puid):
     """
     calculate retry cycles and intervals accordding to database setting
     :param conn: database connection object
-    :return: dictionary of {retry, wait}
+    :return: tuple of (retry, wait)
     """
     sql = f"SELECT schedule_sec, retry FROM policies WHERE uid = '{puid}'"
     _sch, _rtr = db_select(conn, sql)[0]
     _wait = int(_sch / (_rtr + 1))   # the interval between retries
-    return {'retry': _rtr, 'wait': _wait}
+    return (_rtr,_wait)
 
 
-def exec_task_routine(_retry, _wait):
+def exec_task_routine(jobs_home, db, task_uid, _retry, _wait):
     
     # alert handeling
     def raise_alert(_string):
@@ -95,6 +95,7 @@ def exec_task_routine(_retry, _wait):
 
     i = 1
     results_ok = False
+    conn = create_connection(db)
     while i < _retry + 1:
         print(f"\n[ Task {task_uid[0]}: run # {i} ]")
         sql = f""" SELECT j.name, j.id, j.destination, j.metadata, j.content 
@@ -114,34 +115,29 @@ def exec_task_routine(_retry, _wait):
                 response = json.loads(response.replace("\'", "\""))   # converting string to dictionary
                 for k,v in response.items():
                     if k == job_obj_type:
-                        ### DEBUG ###
-                        job_results.append(random.randrange(100,101))
-                        print(f"RECORDED {job_obj_type} ENTRIES INTO JOB_RESULTS => {job_results}")
-
-                        #job_results.append(int(v['entries']))
+                        job_results.append(int(v['entries']))
                         influx_data = {'env': job_dest_env, 'obj_type': job_obj_type, 'count': v['entries']}
-            write_to_influx('mydb', influx_data)
-            # check if results vary to raise an alert    
-            if job_results.count(job_results[0]) == len(job_results):
-                print("RESULTS MATCH!")
-                results_ok = True
-                break
+                        write_to_influx('mydb', influx_data)
+                print(f"job_results --> {job_results}")
+        # check if results vary to raise an alert    
+        if job_results.count(job_results[0]) == len(job_results):
+            results_ok = True
+            break
         if results_ok: break
         i += 1
         if i < _retry + 1: sleep(_wait)
         
     # if results not ok we raise an alert
     if not results_ok:    
-        raise_alert(f"{datetime.datetime.now()} [FAILED] object type: {job_obj_type}")
-
+        raise_alert(f"{datetime.datetime.now()} object type: {job_obj_type} validation failed")
+    else:
+         print(f"{datetime.datetime.now()} object type: {job_obj_type} validation ok")
 
 
 if __name__ == '__main__':
-    os.environ['COCKPIT_HOME']
     config_yaml = f"{os.environ['COCKPIT_HOME']}/config/config.yaml"
-    policies_home = f"{os.environ['COCKPIT_HOME']}/policies"
-    policies_exec_home = f"{policies_home}/exec"
     jobs_home = f"{os.environ['COCKPIT_HOME']}/jobs"
+    multiprocessing.set_start_method('spawn')
 
     # get policy schedule from file name
     policy_uid = '.'.join(os.path.basename(__file__).split('.')[:-1])
@@ -156,13 +152,18 @@ if __name__ == '__main__':
     conn = create_connection(cockpit_db)
 
     # get retry and wait params
-    for _retry, _wait in calculate_retries(conn, policy_uid): pass
-    rand_wait = _wait + random.randrange(-2, 2)  # we randomize wait time by offset of 2
-    process = multiprocessing.set_start_method('spawn')
-    process(name='tasksMain', daemon=True)
-    sql = f"SELECT task_uid FROM policies WHERE uid = '{policy_uid}';"
+    _retry, _wait = calculate_retries(conn, policy_uid)
+    rand_wait = _wait + randrange(-2,2)  # we randomize wait time by offset
+
     # creating child processes by task_uid
-    processes = [process(target=exec_task_routine, args=(_retry, rand_wait)) for task_uid in db_select(conn, sql)]
+    sql = f"SELECT task_uid FROM policies WHERE uid = '{policy_uid}';"
+    processes = [
+        multiprocessing.Process(
+            target=exec_task_routine, 
+            args=(jobs_home, cockpit_db, task_uid, _retry, rand_wait), 
+            daemon=True
+            ) for task_uid in db_select(conn, sql)[0]
+    ]
     # start child processes
     for p in processes:
         p.start()
