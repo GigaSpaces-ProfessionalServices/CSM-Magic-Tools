8cc165ca0ae3527adcbd67347e6144959af65e35
segalalon <segalalon@gmail.com>
2022-11-14 13:54:02 +0200

    fix sql bug dot in object name + table improved

--- recovery_monitor/recovery_monitor.py
+++ recovery_monitor/recovery_monitor.py
@@ -108,13 +108,13 @@ def get_cur_percent():
 def get_sqlite_object_count(the_host, instance_id, otype=None):
     sqlite_cmd = f"sqlite3 /dbagigadata/tiered-storage/bllspace/sqlite_db_{space_name}_container{instance_id}:{space_name}"
     if 'type' in arguments:
-        select_cmd = f"'select count(*) from {obj_type}'"
+        select_cmd = 'select count(*) from \\"' + obj_type + '\\"'
     else:
         select_cmd = f"'{build_sqlite_query(list_types())}'"
     remote_cmd = f'"{sqlite_cmd} {select_cmd}"'
     sh_cmd = f"ssh {the_host} {remote_cmd}"
-    the_response = str(subprocess.run([sh_cmd], shell=True, stdout=subprocess.PIPE).stdout)
-    return int(the_response.strip("\\n'").strip("b'"))
+    the_response = subprocess.run([sh_cmd], shell=True, stdout=subprocess.PIPE).stdout.decode()
+    return int(the_response)
 
 
 def check_space_exists(space):
@@ -170,23 +170,40 @@ def check_sqlite_exists(the_host, instance_id):
 def build_sqlite_query(types):
     query = "select ("
     for t in types:
-        query += f"select count(*) from '{t}') + ("
+        query += 'select count(*) from \\"' + t + '\\") + ('
     return query.rstrip(' + (')
 
 
 def print_table_row(type, dl=[]):
-    table_width = 112
+    table_width = 118
     h_border = "=" * table_width
     r_border = "-" * table_width
-    h1 = ['Partition #', 'Expected Records', 'Current Records', 'Progress %', 'Record Gap', 'Status']
-    h2 = ['Host', '# Records', f"{Back.BLUE}{Fore.BLACK}P{Style.RESET_ALL}/{Back.LIGHTWHITE_EX}{Fore.BLACK}B{Style.RESET_ALL}"]
+    h1 = [
+        'Partition #',
+        'Expected Records',
+        'Current Records',
+        'Progress %',
+        'Record Gap',
+        'Status'
+    ]
+    P = f"{Back.BLUE}{Fore.BLACK}P{Style.RESET_ALL}"
+    B = f"{Back.LIGHTWHITE_EX}{Fore.BLACK}B{Style.RESET_ALL}"
+    h2 = ['Host', '# Records', f"{P}/{B}"]
     if type == 'header':
-        row = f"{h_border}\n|{h1[0]:^13}|{h1[1]:^27}|{h1[2]:^27}|{h1[3]:^12}|{h1[4]:^12}|{h1[5]:^14}|\n{h_border}\n"
-        row += f"|{' ':^13}|{h2[0]:^13}|{h2[1]:^9}|{h2[2]:^3}|{h2[0]:^13}|{h2[1]:^9}|{h2[2]:^3}|{'':^12}|{'':^12}|{'':^14}|\n{h_border}"
+        row = f"{h_border}\n|{h1[0]:^13}|" + \
+            f"{h1[1]:^30}|{h1[2]:^30}|" + \
+            f"{h1[3]:^12}|{h1[4]:^12}|{h1[5]:^14}|\n{h_border}\n"
+        row += f"|{' ':^13}|" + \
+            f"{h2[0]:^16}|{h2[1]:^9}|{h2[2]:^3}|" + \
+            f"{h2[0]:^16}|{h2[1]:^9}|{h2[2]:^3}|" + \
+            f"{'':^12}|{'':^12}|{'':^14}|\n{h_border}"
     if type == 'seperator':
         row = f"{h_border}"
     if type == 'row':
-        row = f"|{dl[0]:^13}|{dl[1]:^13}|{dl[2]:^9}|{dl[3]:^3}|{dl[4]:^13}|{dl[5]:^9}|{dl[6]:^3}|{dl[7]:^12}|{dl[8]:^12}|{dl[9]:<14}|"
+        row = f"|{dl[0]:^13}|" + \
+            f"{f'{dl[1][0:13]:^13}...'}|{dl[2]:^9}|{dl[3]:^3}|" + \
+            f"{f'{dl[4][0:13]:^13}...'}|{dl[5]:^9}|{dl[6]:^3}|" + \
+            f"{dl[7]:^12}|{dl[8]:^12}|{dl[9]:<14}|"
         if debug:
             row += f"{dl[10]} ; {dl[11]}"
     print(row)
@@ -212,140 +229,144 @@ if __name__ == '__main__':
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
     arguments = argument_parser()
     if arguments:
-        space_name = arguments['space_name']
-        # check REST status and set operational manager
-        with open(odsx_hosts_config, 'r') as y:
-            hosts = yaml.safe_load(y)
-        manager_hosts = hosts['servers']['manager']
-        managers = []
-        for mgr in manager_hosts.values():
-            managers.append(mgr)
-        # configure authentication
-        auth = {}
-        if is_env_secured(managers[0]):
-            auth = get_auth(managers[0])
-        else:
-            auth['user'], auth['pass'] = '', ''
-        rest_ok_hosts = []
-        for mgr in managers:
-            url = f'http://{mgr}:{defualt_port}/v2/index.html'
-            if is_restful_ok(url):
-                rest_ok_hosts.append(mgr)
-        if len(rest_ok_hosts) == 0:
-            print('REST status: DOWN')
-            exit(1)
-        else:
-            # we use 1st host from rest_ok_hosts
-            manager = rest_ok_hosts[0]
-        if 'unattended' in arguments:
-            unattended = True
-        else:
-            unattended = False
-            subprocess.run(['clear'])
-            print(pyfiglet.figlet_format("Recovery Monitor"))
-        # check if space name is ok
-        if not check_space_exists(space_name):
-            print(f"ERROR: space name '{space_name}' could not be found.")
-            exit(1)
-        if 'list' in arguments:
-            print_types(list_types())
-            exit(0)
-        if 'type' in arguments:
-            obj_type = arguments['type']
-            # check if type is ok
-            if not check_type_exists(obj_type):
-                print(f"[ ERROR ]\nselected type '{obj_type}' does exist. Please choose one of the following available types:\n")
+        try:
+            space_name = arguments['space_name']
+            # check REST status and set operational manager
+            with open(odsx_hosts_config, 'r') as y:
+                hosts = yaml.safe_load(y)
+            manager_hosts = hosts['servers']['manager']
+            managers = []
+            for mgr in manager_hosts.values():
+                managers.append(mgr)
+            # configure authentication
+            auth = {}
+            if is_env_secured(managers[0]):
+                auth = get_auth(managers[0])
+            else:
+                auth['user'], auth['pass'] = '', ''
+            rest_ok_hosts = []
+            for mgr in managers:
+                url = f'http://{mgr}:{defualt_port}/v2/index.html'
+                if is_restful_ok(url):
+                    rest_ok_hosts.append(mgr)
+            if len(rest_ok_hosts) == 0:
+                print('REST status: DOWN')
+                exit(1)
+            else:
+                # we use 1st host from rest_ok_hosts
+                manager = rest_ok_hosts[0]
+            if 'unattended' in arguments:
+                unattended = True
+            else:
+                unattended = False
+                subprocess.run(['clear'])
+                print(pyfiglet.figlet_format("Recovery Monitor"))
+            # check if space name is ok
+            if not check_space_exists(space_name):
+                print(f"ERROR: space name '{space_name}' could not be found.")
+                exit(1)
+            if 'list' in arguments:
                 print_types(list_types())
                 exit(0)
-            else:
-                print(f"Genarating report for type: {obj_type}")
-        if 'debug' in arguments:
-            debug = True
-        else:
-            debug = False
-
-        # print table header
-        print_table_row('header', "")
-
-        # get number of partitions in space
-        headers = {'Accept': 'application/json'}
-        url = f"http://{manager}:{defualt_port}/v2/spaces"
-        response_data = requests.get(url, auth=(auth['user'], auth['pass']), headers=headers, verify=False)
-        for space in response_data.json():
-            if space['name'] == space_name:
-                num_partitions = space['topology']['partitions']
-        url = f"http://{manager}:{defualt_port}/v2/spaces/{space_name}/instances"
-        response = requests.get(url, auth=(auth['user'], auth['pass']), headers=headers, verify=False)
-        p_total_count, b_total_count = 0, 0
-        for num in range(1,num_partitions + 1):
-            # get instances params
-            primary, backup = False, False
-            P = Back.BLUE + Fore.BLACK + " P " + Style.RESET_ALL
-            B = Back.LIGHTWHITE_EX + Fore.BLACK + " B " + Style.RESET_ALL
-            for item in response.json():
-                if f"{space_name}~{num}_" in item['id']:
-                    if item['mode'] == 'PRIMARY':
-                        p_id = item['id']
-                        p_host_id = item['hostId']
-                        p_sqlite_id = get_sqlite_id(item['id'])
-                        if 'obj_type' in locals():
-                            p_count = get_sqlite_object_count(p_host_id, p_sqlite_id, obj_type)
-                            p_total_count += p_count
-                        else:
-                            p_count = get_sqlite_object_count(p_host_id, p_sqlite_id)
-                            p_total_count += p_count
-                        primary = True
-                    if item['mode'] == 'BACKUP':
-                        b_id = item['id']
-                        b_host_id = item['hostId']
-                        b_sqlite_id = get_sqlite_id(item['id'])
-                        if 'obj_type' in locals():
-                            b_count = get_sqlite_object_count(b_host_id, b_sqlite_id, obj_type)
-                            b_total_count += b_count
-                        else:
-                            b_count = get_sqlite_object_count(b_host_id, b_sqlite_id)
-                            b_total_count += b_count
-                        backup = True
-                if primary and backup:
-                    break
-            if not primary:
-                p_id = 'NONE'
-            if not backup:
-                b_id = 'NONE'
-            if p_count < b_count:
-                progress_prct = f"{floor(p_count / b_count * 100)}%"
-                progress_gap = b_count - p_count
-                status = Fore.LIGHTYELLOW_EX + "In Progress..." + Style.RESET_ALL
-                if debug:
-                    rdata = [num, b_host_id, b_count, B, p_host_id, p_count, P, progress_prct, progress_gap,status, f"B_ID:{b_id}", f"P_ID:{p_id}"]
+            if 'type' in arguments:
+                obj_type = arguments['type']
+                # check if type is ok
+                if not check_type_exists(obj_type):
+                    print(f"[ ERROR ]\nselected type '{obj_type}' does exist. Please choose one of the following available types:\n")
+                    print_types(list_types())
+                    exit(0)
                 else:
-                    rdata = [num, b_host_id, b_count, B, p_host_id, p_count, P, progress_prct, progress_gap, status]
+                    print(f"Genarating report for type: {obj_type}")
+            if 'debug' in arguments:
+                debug = True
             else:
-                if p_count > 0:
-                    progress_prct = f"{floor(b_count / p_count * 100)}%"
-                    progress_gap = p_count - b_count
-                    if floor(b_count / p_count * 100) < 100:
-                        status = Fore.YELLOW + "In Progress..." + Style.RESET_ALL
+                debug = False
+
+            # print table header
+            print_table_row('header', "")
+
+            # get number of partitions in space
+            headers = {'Accept': 'application/json'}
+            url = f"http://{manager}:{defualt_port}/v2/spaces"
+            response_data = requests.get(url, auth=(auth['user'], auth['pass']), headers=headers, verify=False)
+            for space in response_data.json():
+                if space['name'] == space_name:
+                    num_partitions = space['topology']['partitions']
+            url = f"http://{manager}:{defualt_port}/v2/spaces/{space_name}/instances"
+            response = requests.get(url, auth=(auth['user'], auth['pass']), headers=headers, verify=False)
+            p_total_count, b_total_count = 0, 0
+            for num in range(1,num_partitions + 1):
+                # get instances params
+                primary, backup = False, False
+                P = Back.BLUE + Fore.BLACK + " P " + Style.RESET_ALL
+                B = Back.LIGHTWHITE_EX + Fore.BLACK + " B " + Style.RESET_ALL
+                for item in response.json():
+                    if f"{space_name}~{num}_" in item['id']:
+                        if item['mode'] == 'PRIMARY':
+                            p_id = item['id']
+                            p_host_id = item['hostId']
+                            p_sqlite_id = get_sqlite_id(item['id'])
+                            if 'obj_type' in locals():
+                                p_count = get_sqlite_object_count(p_host_id, p_sqlite_id, obj_type)
+                                p_total_count += p_count
+                            else:
+                                p_count = get_sqlite_object_count(p_host_id, p_sqlite_id)
+                                p_total_count += p_count
+                            primary = True
+                        if item['mode'] == 'BACKUP':
+                            b_id = item['id']
+                            b_host_id = item['hostId']
+                            b_sqlite_id = get_sqlite_id(item['id'])
+                            if 'obj_type' in locals():
+                                b_count = get_sqlite_object_count(b_host_id, b_sqlite_id, obj_type)
+                                b_total_count += b_count
+                            else:
+                                b_count = get_sqlite_object_count(b_host_id, b_sqlite_id)
+                                b_total_count += b_count
+                            backup = True
+                    if primary and backup:
+                        break
+                if not primary:
+                    p_id = 'NONE'
+                if not backup:
+                    b_id = 'NONE'
+                if p_count < b_count:
+                    progress_prct = f"{floor(p_count / b_count * 100)}%"
+                    progress_gap = b_count - p_count
+                    status = Fore.LIGHTYELLOW_EX + "In Progress..." + Style.RESET_ALL
+                    if debug:
+                        rdata = [num, b_host_id, b_count, B, p_host_id, p_count, P, progress_prct, progress_gap,status, f"B_ID:{b_id}", f"P_ID:{p_id}"]
                     else:
+                        rdata = [num, b_host_id, b_count, B, p_host_id, p_count, P, progress_prct, progress_gap, status]
+                else:
+                    if p_count > 0:
+                        progress_prct = f"{floor(b_count / p_count * 100)}%"
+                        progress_gap = p_count - b_count
+                        if floor(b_count / p_count * 100) < 100:
+                            status = Fore.YELLOW + "In Progress..." + Style.RESET_ALL
+                        else:
+                            status = Fore.GREEN + "Synchronized  " + Style.RESET_ALL
+                    else:
+                        progress_prct = "100%"
+                        progress_gap = 0
                         status = Fore.GREEN + "Synchronized  " + Style.RESET_ALL
-                else:
-                    progress_prct = "100%"
-                    progress_gap = 0
-                    status = Fore.GREEN + "Synchronized  " + Style.RESET_ALL
-                if debug:
-                    rdata = [num, p_host_id, p_count, P, b_host_id, b_count, B, progress_prct, progress_gap, status, f"P_ID:{p_id}", f"B_ID:{b_id}"]
-                else:
-                    rdata = [num, p_host_id, p_count, P, b_host_id, b_count, B, progress_prct, progress_gap, status]
-            print_table_row('row', rdata)
-            rdata.clear()
-        print_table_row('seperator', "")
-        p_total_display = f"{Fore.MAGENTA}{p_total_count:,}{Style.RESET_ALL}"
-        b_total_display = f"{Fore.MAGENTA}{b_total_count:,}{Style.RESET_ALL}"
-        if 'obj_type' in locals():
-            print(f"{'Total number of records in PRIMARY instances:':<46}{p_total_display:<10} (for type: '{obj_type}')")
-            print(f"{'Total number of records in BACKUP instances:':<46}{b_total_display:<10} (for type: '{obj_type}')")
-        else:
-            print(f"{'Total number of records in PRIMARY instances:':<46}{p_total_display:<10}")
-            print(f"{'Total number of records in BACKUP instances:':<46}{b_total_display:<10}")
+                    if debug:
+                        rdata = [num, p_host_id, p_count, P, b_host_id, b_count, B, progress_prct, progress_gap, status, f"P_ID:{p_id}", f"B_ID:{b_id}"]
+                    else:
+                        rdata = [num, p_host_id, p_count, P, b_host_id, b_count, B, progress_prct, progress_gap, status]
+                print_table_row('row', rdata)
+                rdata.clear()
+            print_table_row('seperator', "")
+            p_total_display = f"{Fore.MAGENTA}{p_total_count:,}{Style.RESET_ALL}"
+            b_total_display = f"{Fore.MAGENTA}{b_total_count:,}{Style.RESET_ALL}"
+            if 'obj_type' in locals():
+                print(f"{'Total number of records in PRIMARY instances:':<46}{p_total_display:<10} (for type: '{obj_type}')")
+                print(f"{'Total number of records in BACKUP instances:':<46}{b_total_display:<10} (for type: '{obj_type}')")
+            else:
+                print(f"{'Total number of records in PRIMARY instances:':<46}{p_total_display:<10}")
+                print(f"{'Total number of records in BACKUP instances:':<46}{b_total_display:<10}")
+        except (KeyboardInterrupt, SystemExit):
+            print('\n')
+            exit(0)
 exit(0)
 
