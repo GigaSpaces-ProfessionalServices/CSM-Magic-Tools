6dae6f190bf087e80791503226bd204adb6e5591
rojosh67 <96982659+rojosh67@users.noreply.github.com>
2022-12-26 22:33:47 +0200

    Update host-yaml.sh

--- host-yaml/host-yaml.sh
+++ host-yaml/host-yaml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 role_found=0
-[[ $# -eq 0 ]] && { echo "No parameter given" ; exit 1 ; }
+[[ $# -ne 1 ]] && { echo -e "\nGive one parameter which should be a name of a role.\n" ; exit 1 ; }
 
 while read line ; do
 [[ $(echo "${line}" | grep "^ *${1} *:" >/dev/null 2>&1 ; echo $? ) -eq 0 ]] && { role_found=1 ; continue ; }
