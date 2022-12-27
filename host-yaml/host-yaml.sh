e60febaacd5bbda09f550fb2f1806baf99a05c39
rojosh67 <96982659+rojosh67@users.noreply.github.com>
2022-12-27 15:22:59 +0200

    Update host-yaml.sh

--- host-yaml/host-yaml.sh
+++ host-yaml/host-yaml.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-role_found=0
 [[ $# -ne 1 ]] && { echo -e "\nGive one parameter which should be a name of a role.\n" ; exit 1 ; }
 
+ENV_COFIG=/dbagigashare/current/odsx
+role_found=0
 while read line ; do
 [[ $(echo "${line}" | grep "^ *${1} *:" >/dev/null 2>&1 ; echo $? ) -eq 0 ]] && { role_found=1 ; continue ; }
 if [[ $role_found -ne 0 && $(echo "${line}" | grep -w '^ *host[0-9]* *:' >/dev/null 2>&1 ; echo $? ) -eq 0 ]] ; then
@@ -9,4 +10,4 @@ if [[ $role_found -ne 0 && $(echo "${line}" | grep -w '^ *host[0-9]* *:' >/dev/n
 else
   role_found=0
 fi
-done < <(cat $ODSXARTIFACTS/odsx/host.yaml | sed '/^ *$/d')
+done < <(cat ${ENV_COFIG}/host.yaml | sed '/^ *$/d')
