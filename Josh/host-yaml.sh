9ff9e584c7025b4b2d27d794aa80a95fa5281bec
rojosh67 <96982659+rojosh67@users.noreply.github.com>
2023-05-16 23:15:56 +0300

    Update host-yaml.sh

--- Josh/host-yaml.sh
+++ Josh/host-yaml.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-# version 2.00
-# build 2023-05-01
 
 role_found=0
 _ROLE=""
 
 shortcuts() {
-  case $1 in 
-    "-m"|"m"|"manager" ) _ROLE=manager ;;
-    "-s"|"s"|"space" ) _ROLE=space ;;
-    "-d"|"d"|"c"|"dataIntegration" ) _ROLE=dataIntegration ;;
-    "-g"|"g"|"grafana" ) _ROLE=grafana ;;
-    "-i"|"i"}"influxdb" ) _ROLE=influxdb ;;
-    "-na"|"na"|"nb_applicative" ) _ROLE=nb_applicative ;;
-    "-nm"|"nm"|"nb_management" ) _ROLE=nb_management ;;
-    "-dvs"|"dvs"|"data_validator_server" ) _ROLE=data_validator_server ;;
-    "-dva"|"dva"|"data_validator_agent" ) _ROLE=data_validator_agent ;;
-    "-p"|"p"|"pivot" ) _ROLE=pivot ;;
+  case $1 in
+    "m"|"manager" ) _ROLE=manager ;;
+    "s"|"space" ) _ROLE=space ;;
+    "d"|"c"|"dataIntegration" ) _ROLE=dataIntegration ;;
+    "g"|"grafana" ) _ROLE=grafana ;;
+    "i"}"influxdb" ) _ROLE=influxdb ;;
+    "na"|"nb_applicative" ) _ROLE=nb_applicative ;;
+    "nm"|"nb_management" ) _ROLE=nb_management ;;
+    "dvs"|"data_validator_server" ) _ROLE=data_validator_server ;;
+    "dva"|"data_validator_agent" ) _ROLE=data_validator_agent ;;
+    "p"|"pivot" ) _ROLE=pivot ;;
     * ) _ROLE=""
   esac
 }
@@ -28,10 +26,26 @@ cat << EOF
     Usage: $0
 
     PARAMETERS:
-      -r     - Display all role names
-      -l     - Display all role names including their hosts
-      -A          - Display all hostnames
+      -h          - Show usage
+      -r          - Display all roles
+      -A          - Display all hosts
+      -l          - Display all roles and hosts
       <role_name> - Display hostnames of type <role_name>
+
+    DEFAULT:
+      Show usage
+
+    A <role_name> can be one of the following:
+      m | manager
+      s | space
+      d | c | dataIntegration
+      g | grafana
+      i | influxdb
+      na | nb_applicative
+      nm | nb_management
+      dvs | data_validator_server
+      dva | data_validator_agent
+      p | pivot
 
 EOF
 }
@@ -46,19 +60,41 @@ display_hosts_per_role() {
     else
       role_found=0
     fi
-  done < <(cat ${ENV_CONFIG}/host.yaml | sed '/^ *$/d')
+  done < <( sed '/servers :/,${//d};/^ *$/d' ${ENV_CONFIG}/host.yaml )
+}
+
+show_no_dups() {
+  local all_hosts=()
+  while read h ; do
+    local found=0
+    for (( i=0 ; i < ${#all_hosts[@]} ; i++ )) ; do
+      [[ "${h}" == "${all_hosts[${i}]}" ]] && { found=1 ; break ; }
+    done
+    [[ $found -ne 1 ]] && { echo $h ; all_hosts=( ${all_hosts[@]} $h ) ; }
+  done < <( sed -En 's/ //g ; /x\.x\./d ; s/(.*host.*: *)(.*)$/\2/p' ${ENV_CONFIG}/host.yaml )
 }
 
 # Check for parameters
 host_menu() {
   [[ $# -lt 1 ]] && show_usage
-  [[ ! -f /dbagigashare/env_config/host.yaml && ! -f /dbagigashare/dr/env_config/host.yaml && ! -f /dbagigashare/prd/env_config/host.yaml ]] && ENV_CONFIG=/dbagiga/env_config
+  if [[ ! -f ${ENV_CONFIG}/host.yaml ]] ; then
+    local hostyaml="${ENV_CONFIG}/host.yaml"
+    if [[ -f /dbagiga/host.yaml ]] ; then
+      ENV_CONFIG=/dbagiga/env_config
+    elif [[ -f /giga/host.yaml ]] ; then
+      ENV_CONFIG=/giga/env_config
+    else
+      echo -e "\nFile host.yaml not found!\n" ; exit 1
+    fi
+    echo -e "\n${hostyaml}\n not found!\n" ; sleep 1
+  fi
   while [[ $# -gt 0 ]] ; do
     case $1 in
-      "-h" | "h" | "" ) show_usage ;;
-      "-r" | "r" ) tail -n +2 ${ENV_CONFIG}/host.yaml | sed '/^ *$/d' | sed -n '/: *$/p' | sed 's/ *: *$//' ;;
-      "-l" | "l" ) tail -n +2 ${ENV_CONFIG}/host.yaml | sed '/^ *$/d' ;;
-      "-A" | "A" ) sed -En 's/(.*host.*: *)(.*)$/\2/p' ${ENV_CONFIG}/host.yaml | tr -d ' ' | sort -u ;;
+      "-h" | "" ) show_usage ;;
+      "-l" ) sed '/servers :/,${//d};/^ *$/d' ${ENV_CONFIG}/host.yaml ;;
+      "-r" ) sed '/servers :/,${//d} ; /^ *$/d ; /host[0-9]*.*:/d ; /: *$/!d ; s/ *: *$// ' ${ENV_CONFIG}/host.yaml ;;
+      # Out put all unique hosts/ips.
+      "-A" ) show_no_dups ;;
       * ) display_hosts_per_role $1 ;;
     esac
     shift
