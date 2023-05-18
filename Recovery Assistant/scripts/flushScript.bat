dac2aab23af5806f2e9d81846bef045bdb0501da
Alon Segal <segalalon@gmail.com>
2023-05-18 16:38:09 +0300

    added cwd for full path call

--- Recovery Assistant/scripts/flushScript.bat
+++ Recovery Assistant/scripts/flushScript.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-call setenvredolog.bat
+call "%~dp0\setenvredolog.bat"
 rem set spaceName=dataExampleSpace
  
 java -jar redolog-client-1.0-SNAPSHOT-jar-with-dependencies.jar FlushRedoLogToDisk --spaceName=%spaceName% 
