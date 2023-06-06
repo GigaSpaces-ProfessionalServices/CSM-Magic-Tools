6d7f65296cbeaac22dc12634649d0a228b3e96f4
Alon Segal <segalalon@gmail.com>
2023-06-06 14:11:40 +0300

    deleted wincmd related files - converted to python

--- Recovery Assistant/scripts/flushScript.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-call "%~dp0\setenvredolog.bat"
-rem set spaceName=dataExampleSpace
- 
-java -jar redolog-client-1.0-SNAPSHOT-jar-with-dependencies.jar FlushRedoLogToDisk --spaceName=%spaceName% 
--- Recovery Assistant/scripts/spaceReplay.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-call setenvredolog.bat
-rem set spaceName=dataExampleSpace
-
-ReadRedoLogContents.exe --spaceName=%spaceName% --lookupLocators=%lookupLocators% --lookupGroups=%lookupGroups% --redoLogYaml=%redoLogYaml% --assemblyFileName=%assemblyFileName%
