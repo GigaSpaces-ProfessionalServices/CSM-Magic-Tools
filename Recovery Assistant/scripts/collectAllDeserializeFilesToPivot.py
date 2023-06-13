81dec892592c7b00264f996e0a95832c9120b120
Alon Segal <segalalon@gmail.com>
2023-06-13 09:47:33 +0300

    added create for folder AllDeserializedFiles

--- Recovery Assistant/scripts/collectAllDeserializeFilesToPivot.py
+++ Recovery Assistant/scripts/collectAllDeserializeFilesToPivot.py
@@ -2,6 +2,7 @@
 # *-* coding: utf-8 *-*
 
 import subprocess
+import os
 from setenvredolog import *
 
 jarFilePath = str(jarFilePath).replace("\\","\\\\")
@@ -9,7 +10,7 @@ gsHome = str(gsHome).replace("\\","\\\\") + '\\\\'
 scriptLocation = str(scriptLocation).replace("\\","\\\\") + '\\\\'
 targetPath = str(targetPath).replace("\\","\\\\") + '\\\\'
 targetPathBaseDir = str(targetPathBaseDir).replace("\\","\\\\")
-deserializeFullPath = str(deserializeFullPath).replace("\\","\\\\")
+deserializeFullPath = str(deserializeFullPath).replace("\\","\\\\") + '\\\\'
 
 jCMD = f'java -jar "{jarFilePath}" RemoteDownloadfiles \
 --spaceName={spaceName} \
@@ -19,5 +20,11 @@ jCMD = f'java -jar "{jarFilePath}" RemoteDownloadfiles \
 --targetPathBaseDir="{targetPathBaseDir} " \
 --deserializeFullPath="{deserializeFullPath} "'
 
-subprocess.call(jCMD, shell=True)
+# create target folder if not exist
+target_path = str(pathlib.PurePath(targetPathBaseDir).joinpath('AllDeserializedFiles'))
+if not os.path.exists(target_path):
+    os.makedirs(target_path)
+
+subprocess.run(jCMD, shell=True)
+print()
 subprocess.call('pause', shell=True)
