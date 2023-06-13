1da4eaf2a76d576355aff6b1ed8c9682574259b2
Alon Segal <segalalon@gmail.com>
2023-06-13 09:49:02 +0300

    changes to pathlib usage + vars for resource sync

--- Recovery Assistant/scripts/setenvredolog.py
+++ Recovery Assistant/scripts/setenvredolog.py
@@ -12,15 +12,17 @@ deserializeScriptName = "deserializeScript.py"
 jarFileName = 'redolog-client-1.0-SNAPSHOT-jar-with-dependencies.jar'
 
 # setting paths accordding to this script location
-gsHome = pathlib.PurePath(__file__).parent.parent.parent
 raHome = pathlib.PurePath(__file__).parent.parent
-scriptLocation = pathlib.PurePath(raHome).joinpath('scripts')
-resourceLocation = pathlib.PurePath(raHome).joinpath('resources')
-jarFilePath = pathlib.PurePath(resourceLocation).joinpath(jarFileName)
-sourcePath = pathlib.PurePath(gsHome).joinpath('Work/redo-log', spaceName)
-targetPath = pathlib.PurePath(gsHome).joinpath('backup/work/redo-log', spaceName)
-targetPathBaseDir = pathlib.PurePath(gsHome).joinpath('backup')
-deserializeFullPath = pathlib.PurePath(gsHome).joinpath('backup')
+gsHome = pathlib.PurePath(raHome).parent
+scriptLocation = raHome.joinpath('scripts')
+resourceLocation = raHome.joinpath('resources')
+resourceAdminShare = str(resourceLocation.drive).replace(':','$')
+resourceNetPath = f"{resourceAdminShare}{str(resourceLocation).replace(resourceLocation.drive , '')}"
+jarFilePath = resourceLocation.joinpath(jarFileName)
+sourcePath = gsHome.joinpath('Work/redo-log', spaceName)
+targetPath = gsHome.joinpath('backup/work/redo-log', spaceName)
+targetPathBaseDir = gsHome.joinpath('backup')
+deserializeFullPath = gsHome.joinpath('backup')
 
-redoLogYaml = str(pathlib.PurePath(gsHome).joinpath('backup','AllDeserializedFiles'))
-assemblyFileName = str(pathlib.PurePath(gsHome).joinpath('Deploy','DataProcessor','GigaSpaces.Examples.ProcessingUnit.Common.dll'))
+redoLogYaml = str(gsHome.joinpath('backup','AllDeserializedFiles'))
+assemblyFileName = str(gsHome.joinpath('Deploy','DataProcessor','GigaSpaces.Examples.ProcessingUnit.Common.dll'))
