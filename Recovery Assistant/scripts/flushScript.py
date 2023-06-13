f77bc31e2ce28b41a3b9a012a53f525eb669a7b2
Alon Segal <segalalon@gmail.com>
2023-06-13 09:43:07 +0300

    deleted since flush is automatic

--- Recovery Assistant/scripts/flushScript.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# *-* coding: utf-8 *-*
-
-import subprocess
-from setenvredolog import *
-
-jCMD = f'java -jar "{str(jarFilePath)}" FlushRedoLogToDisk --spaceName={spaceName}'
-subprocess.call(jCMD, shell=True)
-subprocess.call('pause', shell=True)
--- Recovery Assistant/scripts/scheduleTask.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# *-* coding: utf-8 *-*
-
-import subprocess
-from setenvredolog import *
-
-
-print("\nTBD - scheduling tasks in crontab... \n")
-subprocess.call('pause', shell=True)
