2549d923a5fbc0d70b8152fb8119350897e9c714
Alon Segal <segalalon@gmail.com>
2023-08-15 13:26:24 +0300

    added support for oracle feeders

--- auto_odsx/auto_dataengine.sh
+++ auto_odsx/auto_dataengine.sh
@@ -8,15 +8,14 @@ function usage() {
         err0)
             printf "\nERROR: At least one argument is missing.\n"
             printf "See help [-h, --help] for options.\n"
-            exit 1
             ;;
-        *)
-            printf "\n%-10s\n" "required arguments:"
-            printf "%2s%-20s%-50s\n" "" "-f <FEEDER TYPE>" "the feeder type (db2, mssql ...)"
-            printf "%2s%-20s%-50s\n" "" "-t <TABLE NAME>" "the table name"
-            printf "%2s%-5s%-15s%-50s\n" "" "-h, " "--help" "Display this help screen"
-            printf "\n"
     esac
+    printf "\n%-10s\n" "Arguments:"
+    printf "%2s%-20s%-50s\n" "" "-f <FEEDER TYPE>" "one of: oracle|mssql|db2"
+    printf "%2s%-20s%-50s\n" "" "-t <TABLE NAME>" "the table name"
+    printf "%2s%-5s%-15s%-50s\n" "" "-h, " "--help" "Display this help screen"
+    printf "\n"
+    exit
 }
 
 ### main ###
@@ -25,7 +24,7 @@ date
 # Waiting random miliseconds before executing
 sleep $( bc -l <<< "scale=3 ; ${RANDOM}/32767" )
 
-ZONE=bll
+ZONE=""
 TYPE_SUFFIX="feeder"
 
 while [[ $# -gt 0 ]]; do
@@ -45,19 +44,22 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-# if any argument is missing we exit with code 1
+# if any argument is missing we show usage and exit
 [[ -z $the_feeder ]] || [[ -z $table_name ]] || [[ -z $opt ]] && usage err0
 
-# build table name for dataengine
+# build table name
 if [[ $the_feeder == 'mssql' ]]; then
     table_name="${the_feeder}${TYPE_SUFFIX}_${ZONE}_${table_name}"
+elif [[ $the_feeder == 'oracle' ]]; then
+    table_name="${the_feeder}${TYPE_SUFFIX}_${table_name}"
 else
     table_name="${the_feeder}${TYPE_SUFFIX}_${table_name}"
 fi
-# build feeder name for dataengine
+
+# build feeder name
 the_feeder="${the_feeder}-${TYPE_SUFFIX}"
 
 # run command
-cd /dbagiga/gs-odsx
 echo -e ./odsx.py dataengine $the_feeder $opt $table_name
+cd /dbagiga/gs-odsx
 ./odsx.py dataengine $the_feeder $opt $table_name
