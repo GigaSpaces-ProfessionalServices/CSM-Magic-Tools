27f8e395c82024ee4a169a15dec58e8c9a8ff1e9
niharkapadia <nihar.kapadia@niyantras.com>
2024-02-07 01:06:19 +0530

    Added log line for debug max value issue

--- common/src/main/java/com/gigaspaces/common/task/MaxLocalDateTimeValueTask.java
+++ common/src/main/java/com/gigaspaces/common/task/MaxLocalDateTimeValueTask.java
@@ -15,7 +15,7 @@ import org.openspaces.core.executor.TaskGigaSpace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@SupportCodeChange(id="2")
+@SupportCodeChange(id="3")
 public class MaxLocalDateTimeValueTask implements DistributedTask<LocalDateTime,LocalDateTime> {
 
     private static Logger logger = LoggerFactory.getLogger(MaxLocalDateTimeValueTask.class);
@@ -37,10 +37,18 @@ public class MaxLocalDateTimeValueTask implements DistributedTask<LocalDateTime,
         SQLQuery<SpaceDocument> sqlQuery = new SQLQuery<SpaceDocument>(this.tableName, this.columnName + " < ? AND rownum <= 1");
         sqlQuery.setParameter(1, maxParameter);
         sqlQuery.setProjections(this.columnName);
+        logger.debug("In Execute method");
+        System.out.println("SOP:In Execute method");
         SpaceDocument result = gigaSpace.read(sqlQuery);
+        logger.debug("Result from Execute method: "+result);
+        System.out.println("SOP:Result from Execute method: "+result);
         if(result == null || result.getProperty(this.columnName) == null){
+            logger.debug("Final Result is Null from Execute method");
+            System.out.println("SOP:Final Result is Null from Execute method");
             return null;
         }else {
+            logger.debug("Final Result from Execute method: "+result.getProperty(this.columnName));
+            System.out.println("SOP:Final Result from Execute method: "+result.getProperty(this.columnName));
             return (LocalDateTime) result.getProperty(this.columnName);
         }
     }
@@ -49,11 +57,15 @@ public class MaxLocalDateTimeValueTask implements DistributedTask<LocalDateTime,
     public LocalDateTime reduce(List<AsyncResult<LocalDateTime>> results) throws Exception {
         List<LocalDateTime> resultList = new LinkedList<>();
         logger.debug("MaxLocalDateTimeValueTask Reducer Total Results: "+results.size());
+        System.out.println("SOP:MaxLocalDateTimeValueTask Reducer Total Results: "+results.size());
         for (AsyncResult<LocalDateTime> result : results) {
+            logger.debug("MaxLocalDateTimeValueTask Reducer Individual Result: "+result.getResult());
+            System.out.println("SOP:MaxLocalDateTimeValueTask Reducer Individual Result: "+result.getResult());
             if (result.getException() != null) {
                 throw result.getException();
             }
-            logger.debug("MaxLocalDateTimeValueTask Reducer Individual Result: "+result.getResult());
+            logger.debug("22MaxLocalDateTimeValueTask Reducer Individual Result: "+result.getResult());
+            System.out.println("SOP:22MaxLocalDateTimeValueTask Reducer Individual Result: "+result.getResult());
             if(result.getResult()!=null) {
                 resultList.add(result.getResult());
             }else {
